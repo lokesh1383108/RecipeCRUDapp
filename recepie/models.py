95ff3e9958aa899f4b29ea2273b5c2c74a508e59
Lokesh Kumar sharma <1383lokesh@gmail.com>
2023-10-02 21:13:11 +0530

    trying to display image on frontend , showing backend data to frontend

--- recepie/models.py
+++ recepie/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User 
 
 # Create your models here.
 # Model are actually used to create the database schema, here recipe is the table and recipe_name, recipe_description and recipe_image are the columns
@@ -8,3 +9,4 @@ class Recipe(models.Model):
     recipe_image = models.FileField()
 
 
+ 
